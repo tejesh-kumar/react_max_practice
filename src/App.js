46c3d3fd37a89c4d15b7df3f71c4c3d6da15e080
tejesh <tejeshmuralikrishna@gmail.com>
2019-12-11 23:32:23 +0530

    u-4 v-5 = show Persons div
    map function for each element in array to return a jsx array which gets returned to output persons list dynamically

--- src/App.js
+++ src/App.js
@@ -51,15 +51,11 @@ class App extends Component {
     if(this.state.showPersons) {
       persons = (
         <div>
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age} 
-        click={this.switchNameHandler.bind(this, "tejesh!")}
-        changed={this.nameChangedHandler}>My hobby: Racing</Person>
-
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age} />
+          {
+            this.state.persons.map(person => {
+              return <Person name = {person.name} age = {person.age} />
+            })
+          }
         </div>
       );
     }
