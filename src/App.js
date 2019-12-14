d56f9422cf1bc3db3c99504c5a492d8dcb9c5170
tejesh <tejeshmuralikrishna@gmail.com>
2019-12-14 15:23:35 +0530

    u-5 v-9:- Using css modules if using react-scripts version-1, changes need to be made in config files by ejecting so that we can import css as classes and scope styles to the component. The css classes will be imported with a unique names dynamically generated so that it does not conflict with styles of other components. and if using react-scripts version-2, no need for config changes. We can import css classes as 'import classes from app.modules.css'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 // const StyledButton = styled.button`
@@ -52,20 +52,8 @@ class App extends Component {
   }
 
   render() {
-    // const style = {
-    //   backgroundColor: 'green',
-    //   color: 'white',
-    //   font: 'inherit',
-    //   border: '1px solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer',
-    //   ':hover': {
-    //     backgroundColor: "lightgreen",
-    //     color: "black"
-    //   }
-    // }
-
     let persons = null;
+    let btnClass;
 
     if(this.state.showPersons) {
       persons = (
@@ -83,26 +71,22 @@ class App extends Component {
         </div>
       );
 
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: "black"
-      // };
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
       if(this.state.persons.length <= 2) {
-        classes.push('red');
+        assignedClasses.push(classes.red);
       }
       if(this.state.persons.length <= 1) {
-        classes.push('bold');
+        assignedClasses.push(classes.bold);
       }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
         <h1>Hi, from react App</h1>
-        <p className={classes.join(' ')}>This is working</p>
-        <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <p className={assignedClasses.join(' ')}>This is working</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
