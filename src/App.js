ab77012148f9f54abf1c44d6307bc70622aafd27
tejesh <tejeshmuralikrishna@gmail.com>
2019-12-15 11:13:46 +0530

    u-6 v-6:- ErrorBoundary if a part of code has a chance to fail at run time then a proper msg can be displayed to user by catching the throwed error. Wrap the component in ErrorBoundary tag to catch an error and display to the user.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 // const StyledButton = styled.button`
 //   background-color: green;
@@ -60,12 +61,12 @@ class App extends Component {
         <div>
           {
             this.state.persons.map((person, index) => {
-              return <Person 
+              return <ErrorBoundary key = {person.id}>
+                  <Person 
               name = {person.name} 
               age = {person.age} 
               click = {() => this.deletePersonHandler(index)}
-              changed = {(event) => this.nameChangedHandler(event, person.id)}
-              key = {person.id} />
+              changed = {(event) => this.nameChangedHandler(event, person.id)}/> </ErrorBoundary>
             })
           }
         </div>
