fbbba9f9fb6fbe45225a05a64e7eff02619f5045
tejesh <tejeshmuralikrishna@gmail.com>
2019-12-24 12:20:48 +0530

    u-7 v-8 :- Component lifecycle updation on prop changes Persons.js component executes methods 1. getDerivedStateFromState(props, state) 2. shouldComponentUpdate(nextProps, nextState) 3. render() 4. getSnapshotBeforeUpdate(prevProps, prevState) 5. componentDidUpdate()

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person'
 
 // props contains array of persons which we wnat to convert to array of jsx elements.
-const persons = (props) => {
-    console.log('[Persons.js] rendering...');
-    return props.persons.map((person, index) => <Person name={person.name} 
-    age= {person.age} 
-    click = {() => props.clicked(index)}
-    changed = {(event) => props.changed(event, person.id)} 
-    key = {person.id} />);
+// const persons = (props) => {
+//     console.log('[Persons.js] rendering...');
+//     return props.persons.map((person, index) => <Person name={person.name} 
+//     age= {person.age} 
+//     click = {() => props.clicked(index)}
+//     changed = {(event) => props.changed(event, person.id)} 
+//     key = {person.id} />);
+// }
+
+// export default persons;
+
+class Persons extends Component {
+    // static getDerivedStateFromProps (state, props) {
+    //     console.log('Persons.js [getDerivedStateFromProps]', state);
+    //     return state;
+    // }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('Persons.js [shouldComponentUpdate]');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(previousProps, previousState) {
+        console.log('Persons.js [getSnapshotBeforeUpdate]');
+        return { message: 'Snapshot!' };
+    }
+
+    componentDidUpdate(previousProps, previousState, snapshot) {
+        console.log('Persons.js [componentDidUpdate]');
+        console.log(snapshot);
+    }
+
+    render() {
+        console.log('[Persons.js] rendering...');
+        return this.props.persons.map((person, index) => <Person name={person.name} 
+        age = {person.age} 
+        click = {() => this.props.clicked(index)}
+        changed = {(event) => this.props.changed(event, person.id)} 
+        key = {person.id} />);
+    }   
 }
 
-export default persons;
+export default Persons;
