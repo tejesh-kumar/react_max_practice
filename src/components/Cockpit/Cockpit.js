dc6dd8db0dcee099b4798ddb4920b50b0686e4b1
tejesh <tejeshmuralikrishna@gmail.com>
2019-12-30 12:48:18 +0530

    u-7 v-11 :- Controlling useEffect() Behavior As useEffect() is equivalent to componentDidMount() and componentDidUpdate() combined effect. 1. To run useEffect() only on pageload(default equivalent to componentDidMount()) provide empty array. 2. To run useEffect() on different events, mention variables to subscribe to variable changes to execute useEffect().

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -5,7 +5,14 @@ const cockpit = (props) => {
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // Http request...
-    });
+        setTimeout(() => {
+            alert('Saved data to cloud');
+        }, 1000);
+    }, [props.persons]);   // [a, b, c] the array contains all variables, to which useEffect() executes if they change, 
+  // [] empty array if we want to execute useEffect() on page load once. Equivalent of componentDidMount() only.
+
+
+    //  useEffect()       Different events or functions in different useEffect() calls.
 
     const assignedClasses = [];
     let btnClass = '';
