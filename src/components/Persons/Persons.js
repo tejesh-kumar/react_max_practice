9110eaed9b5b27f855d61807a453be9252b788b0
tejesh <tejeshmuralikrishna@gmail.com>
2020-01-11 14:01:22 +0530

    u-7 v-14 :- shouldComponentUpdate() condition to avoid re-render. shouldComponentUpdate() in persons.js returns true if this.props.persons changes or else returns false as no need to re-render the persons.js(child-component) if there are no changes in persons state. This improves performance. => There is also difference in updating(rendering) the real Dom and virtual Dom whose details can be seen in the next video.

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -21,7 +21,11 @@ class Persons extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
         console.log('Persons.js [shouldComponentUpdate]');
-        return true;
+        if(nextProps.persons !== this.props.persons) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     getSnapshotBeforeUpdate(previousProps, previousState) {
